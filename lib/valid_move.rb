400f297ba5a22daea5c2a81ebfc0bb8439e82e0c
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2020-12-31 20:51:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
 # code your #valid_move? method here
-
+def valid_move?(index)
+  if index < 0 or index > 8
+    return false
+  elsif
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
